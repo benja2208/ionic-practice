28b2a344f22fd481abdd5444838e11b55ed22924
Willian Ribeiro Angelo <agfoccus@gmail.com>
2015-10-02 13:16:07 -0300

    * New intro page specific to android or iphone, which detects the model and changes the image of the device

--- www/app/user/controller/user.intro.ctrl.js
+++ www/app/user/controller/user.intro.ctrl.js
@@ -1,32 +1,45 @@
-(function (window, angular, undefined) {
-  'use strict';
-  angular
-    .module('module.user')
-    .controller('IntroCtrl', function (gettextCatalog) {
-      var vm = this;
+(function (window, angular,ionic, undefined) {
+    'use strict';
+    angular
+        .module('module.user')
+        .controller('IntroCtrl', function (gettextCatalog) {
+            var vm = this;
 
-      vm.slides = [{
-          top: gettextCatalog.getString('Share your most amazing moments'),
-          img: 'img/intro1-phone.png'
-        }, {
-          top: gettextCatalog.getString('Follow your friends and relive'),
-          img: 'img/intro2-phone.png'
-        }, {
-          top: gettextCatalog.getString('Find and follow people around'),
-          img: 'img/intro3-phone.png'
-        }, {
-          top: gettextCatalog.getString('Enjoy the most amazing photos'),
-          img: 'img/intro4-phone.png'
-        }
+            var currentPlatform = ionic.Platform.platform ();
+            console.log (currentPlatform);
 
-      ];
+            if (currentPlatform) {
+                vm.device = (currentPlatform == 'android') ? 'android' : 'iphone';
+            } else {
+                vm.device = 'android';
+            }
 
-      vm.slideIndex = 0;
+            vm.slides = [
+                {
+                    top: gettextCatalog.getString('Share your most amazing moments'),
+                    img: 'img/intro1.jpg'
+                },
+                {
+                    top: gettextCatalog.getString('Follow your friends and relive'),
+                    img: 'img/intro2.jpg'
+                },
+                {
+                    top: gettextCatalog.getString('Find and follow people around'),
+                    img: 'img/intro3.jpg'
+                },
+                {
+                    top: gettextCatalog.getString('Enjoy the most amazing photos'),
+                    img: 'img/intro4.jpg'
+                }
 
-      // Called each time the slide changes
-      vm.slideChanged = function (index) {
-        vm.slideIndex = index;
-      };
+            ];
 
-    });
-})(window, window.angular);
+            vm.slideIndex = 0;
+
+            // Called each time the slide changes
+            vm.slideChanged = function (index) {
+                vm.slideIndex = index;
+            };
+
+        });
+})(window, window.angular, window.ionic);
