317121c631c16754a72930d081170ef0ffddc451
Willian Ribeiro Angelo <agfoccus@gmail.com>
2015-10-04 21:20:36 -0300

    * New intro devices

--- www/app/user/controller/user.intro.ctrl.js
+++ www/app/user/controller/user.intro.ctrl.js
@@ -1,45 +1,44 @@
-(function (window, angular,ionic, undefined) {
-    'use strict';
-    angular
-        .module('module.user')
-        .controller('IntroCtrl', function (gettextCatalog) {
-            var vm = this;
+(function (window, angular, ionic, undefined) {
+  'use strict';
+  angular
+    .module('module.user')
+    .controller('IntroCtrl', function (gettextCatalog) {
+      var vm = this;
 
-            var currentPlatform = ionic.Platform.platform ();
-            console.log (currentPlatform);
+      var currentPlatform = ionic.Platform.platform();
+      console.log(currentPlatform);
 
-            if (currentPlatform) {
-                vm.device = (currentPlatform == 'android') ? 'android' : 'iphone';
-            } else {
-                vm.device = 'android';
-            }
+      if (currentPlatform) {
+        vm.device = (currentPlatform == 'android') ? 'android' : 'iphone';
+      } else {
+        vm.device = 'android';
+      }
 
-            vm.slides = [
-                {
-                    top: gettextCatalog.getString('Share your most amazing moments'),
-                    img: 'img/intro1.jpg'
-                },
-                {
-                    top: gettextCatalog.getString('Follow your friends and relive their moments'),
-                    img: 'img/intro2.jpg'
-                },
-                {
-                    top: gettextCatalog.getString('Find people around you and enjoy your photos'),
-                    img: 'img/intro3.jpg'
-                },
-                {
-                    top: gettextCatalog.getString('Enjoy the most amazing photos'),
-                    img: 'img/intro4.jpg'
-                }
+      vm.slides = [{
+          top: gettextCatalog.getString('Share your most amazing moments'),
+          img: 'img/intro1.jpg'
+        }, {
+          top: gettextCatalog.getString('Follow your friends and relive their moments'),
+          img: 'img/intro2.jpg'
+        }, {
+          top: gettextCatalog.getString('Find people around you and enjoy your photos'),
+          img: 'img/intro3.jpg'
+        }, {
+          top: gettextCatalog.getString('Enjoy the most amazing photos'),
+          img: 'img/intro4.jpg'
+        }, {
+          top: gettextCatalog.getString('Create your Social Photo Share with Ionic Framework!'),
+          img: 'img/intro5.jpg'
+        }
 
-            ];
+      ];
 
-            vm.slideIndex = 0;
+      vm.slideIndex = 0;
 
-            // Called each time the slide changes
-            vm.slideChanged = function (index) {
-                vm.slideIndex = index;
-            };
+      // Called each time the slide changes
+      vm.slideChanged = function (index) {
+        vm.slideIndex = index;
+      };
 
-        });
+    });
 })(window, window.angular, window.ionic);
